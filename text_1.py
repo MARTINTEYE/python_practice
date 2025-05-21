42e5d48a68defc6d3c7d4fa4d63b1ab50410a7d3
MARTIN TEYE <martinteye93@gmail.com>
2025-05-21 21:19:23 +0000

    added a text and py to the branch

---
+++ text_1.py
@@ -0,0 +1,9 @@
+number1 = 4
+number2 = 6
+number3 = 7
+result = number1 + number2 + number3
+difference = number2 - number3 - number1
+product = number1 * number2 * number3
+print(f"Addition of {number1} , {number2}, and {number3} is {result}")
+print(f"Subtraction of {number2} , {number3}, and {number1} is {difference}")
+print(f"Multiplication of {number1} , {number2}, and {number3} is {product}")
