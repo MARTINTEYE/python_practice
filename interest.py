336ccac861b789ebc8fd5ca20ddf206487e65b90
MARTIN TEYE <martinteye93@gmail.com>
2025-05-21 21:52:53 +0000

    2 branch named interest contains simple interest calculation

---
+++ interest.py
@@ -0,0 +1,10 @@
+principal = 2000 # that is $2000
+rate = 0.05 # that is 5%
+time = 5 # that is 5 years
+interest = principal * rate * time
+interest_1 = 5000 # that is $ 5000
+rate_1 = 0.15 # rate is 15%
+time_1 = 7 # that is 7 years
+principal_1 = (interest_1) / (rate_1 * time_1)
+print (f"the interest is {interest}")
+print(f"the principal is {principal_1}")
